776ab98ae8a186184ba494baee6197d5c96e5ffa
001123 <khuongduy1897@gmail.com>
2021-09-12 18:36:29 +0700

    update version

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import 'virtual:windi-devtools'
 // windicss utilities should be the last style import
 import 'virtual:windi-utilities.css'
 import { ViteSSG } from 'vite-ssg'
+// @ts-ignore
 import App from './App.vue'
 // your custom styles here
 import './styles/main.css'
@@ -16,9 +17,11 @@ import './styles/main.css'
 const routes = setupLayouts(generatedRoutes)
 
 // https://github.com/antfu/vite-ssg
-export const createApp = ViteSSG(App, { routes }, (ctx) => {
-  // install all modules under `modules/`
-  Object.values(import.meta.globEager('./modules/*.ts')).map(i =>
-    i.install?.(ctx),
-  )
-})
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  (ctx) => {
+    // install all modules under `modules/`
+    Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
+  },
+)
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,12 +1,13 @@
+/* eslint-disable import/named */
 /* eslint-disable import/no-duplicates */
 
 declare interface Window {
-    // extend the window
+  // extend the window
 }
 
 // with vite-plugin-md, markdowns can be treat as Vue components
 declare module '*.md' {
-    import { ComponentOptions } from 'vue'
-    const component: ComponentOptions
-    export default component
+  import { ComponentOptions } from 'vue'
+  const component: ComponentOptions
+  export default component
 }
